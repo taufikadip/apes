b245767e7971d21700326ed663e0d99891812ecc
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-30 10:57:51 +0700

    task5-6

--- AdvancedTicketingSystem/src/TicketManger.kt
+++ AdvancedTicketingSystem/src/TicketManger.kt
@@ -14,4 +14,28 @@ class TicketManger {
         tickets.add(ticket)
         return ticket
     }
+
+
+    //View Tickets
+    fun viewTickets(){
+        if (tickets.isEmpty()) {
+            println("No tickets available.")
+        } else {
+            println("Current Tickets:")
+            for (i in tickets){
+                println("ID: ${ticket.id}, Description: ${ticket.description}, Priority: ${ticket.priority}, Status: ${ticket.status}, Assignee: ${ticket.assignee?.name ?: "None"}, Due Date: ${ticket.dueDate ?: "None"}")
+            }
+        }
+    }
+
+    //Search Ticket By Id
+    fun searchTicketById(ticketId: Int) {
+        val ticket = tickets.find { it.id == ticketId }
+
+        if (ticket != null) {
+            println("Found Ticket: ID: ${ticket.id}, Description: ${ticket.description}, Priority: ${ticket.priority}, Status: ${ticket.status}, Assignee: ${ticket.assignee?.name ?: "None"}, Due Date: ${ticket.dueDate ?: "None"}")
+        } else {
+            println("Ticket not found.")
+        }
+    }
 }
