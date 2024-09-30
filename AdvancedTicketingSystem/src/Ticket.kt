b734b1c6d71bcf442a38d3ea1a0fb280f3a80900
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-30 10:18:58 +0700

    initiate ticket

---
+++ AdvancedTicketingSystem/src/Ticket.kt
@@ -0,0 +1,12 @@
+data class Ticket(
+    val id: Int
+    var description: String
+    val priority: String,
+    var status: String,
+    var assignee: User? = null,
+    var dueDate: LocalDate? = null
+) { init {
+    require(description.isNotEmpty()){"Deskripsi tidak boleh kosong"}
+    require(priority in listOf("Low","Medium", "High")){"Prioritas yang tersedia antara 'Low', 'Medium', atau 'High'"}
+    require(status in listOf("Open", "In Progress", "Closed")) {"Status seharusnya 'Open', 'In Progress', or 'Closed'."}
+}
